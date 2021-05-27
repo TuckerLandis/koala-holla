08935d851b677ef8ce7baf06d8a9443d24f61374
Chris Klemz <crklemz@gmail.com>
2021-05-27 13:42:44 -0500

    fixed router to koalaRouter on router.get

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -7,7 +7,7 @@ const koalaRouter = express.Router();
 
 
 // GET
-router.get('/', (req, res) => {
+koalaRouter.get('/', (req, res) => {
     let queryText = 'SELECT * FROM "koalas" ORDER BY "name";';
     pool.query(queryText).then(result => {
         res.send(result.rows);
