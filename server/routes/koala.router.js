04f0d1e04a8dc49b2f3cf0e196e889dce31be3b2
Chris Klemz <crklemz@gmail.com>
2021-05-27 13:39:56 -0500

    ajax get and router.get complete. renderDOM and getKoalas created

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,11 +1,21 @@
+const { Router } = require('express');
 const express = require('express');
+const { Pool } = require('pg');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
 
 
 // GET
-
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "koalas" ORDER BY "name";';
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('error getting koalas in router.get', error);
+        res.sendStatus(500);
+    });
+});//end router.get
 
 // POST
 
